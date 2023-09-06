77abb2b073b3b6128e9814357b8608d11aea561d
Fred <ffotre@campus-eni.fr>
2023-09-06 15:38:59 +0200

    init commit

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,6 +11,8 @@ class MainController extends AbstractController
     #[Route('/main', name: 'app_main')]
     public function index(): JsonResponse
     {
+
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/MainController.php',

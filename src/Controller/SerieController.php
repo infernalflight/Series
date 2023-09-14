5f1008f2730b5be3dfa47c2debc785aa9d65a3ab
ffotre <ffotre@campus-eni.fr>
2023-09-14 13:48:31 +0200

    EntityListeners

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -76,6 +76,7 @@ class SerieController extends AbstractController
         $serieForm->handleRequest($request);
 
         if ($serieForm->isSubmitted() && $serieForm->isValid()) {
+            // on passe plutôt par un entityListener
             //$serie->setDateModified(new \DateTime());
             $em->persist($serie);
             $em->flush();
